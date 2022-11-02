db39acc9ad505e6a9fae199e123295adc20c0c2d
kurone02 <fnt.minhduc@gmail.com>
2022-11-02 12:49:53 +0900

    Random world generator

--- testapp/lib/main.dart
+++ testapp/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
@@ -10,17 +11,56 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return MaterialApp(
-      title: "Testing Flutter",
+      title: "Variable Name Generator",
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("Testing Flutter"),
+          title: const Text("Variable Name Generator"),
         ),
-        body: Center(
-          child: Text(wordPair.asPascalCase),
+        body: const Center(
+          child: RandomWords(),
         ),
       ),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({super.key});
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final List<WordPair> suggestions = <WordPair>[];
+    const TextStyle biggerFont = TextStyle(fontSize: 18);
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemBuilder: ((context, index) {
+        if (index.isOdd) {
+          return const Divider(
+            thickness: 2,
+          );
+        }
+
+        // If the index reaches half of the length, double the length
+        final int i = index ~/ 2; // Integer division
+        if (i >= suggestions.length) {
+          if (kDebugMode) {
+            print("ADDED MORE!");
+          }
+          suggestions.addAll(generateWordPairs().take(10));
+        }
+        return ListTile(
+          title: Text(
+            suggestions[i].asPascalCase,
+            style: biggerFont,
+          ),
+        );
+      }),
     );
   }
 }
