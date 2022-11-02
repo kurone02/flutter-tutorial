df20b4e5734ffa4c50cf2554d0f4292c9aad088f
kurone02 <fnt.minhduc@gmail.com>
2022-11-02 12:18:29 +0900

    Hello world!

--- testapp/lib/main.dart
+++ testapp/lib/main.dart
@@ -1,101 +1,26 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'dart:math' as math;
+import 'package:english_words/english_words.dart';
 
-void main() {
+void main(List<String> args) {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  int count = 0;
-  List<Color>? color;
-
-  @override
-  void initState() {
-    color = List<Color>.generate(20, (index) => randomColor());
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
     return MaterialApp(
+      title: "Testing Flutter",
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.green,
           title: const Text("Testing Flutter"),
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            if (kDebugMode) {
-              print("pressed!");
-            }
-            setState(() {
-              count++;
-            });
-          },
-          child: const Icon(Icons.add),
-        ),
-        body: ListView.builder(
-          itemCount: 20,
-          itemBuilder: (_, index) {
-            return Container(
-              color: color?[index],
-              width: 500,
-              height: 500,
-              child: Center(
-                child: Text(
-                  "${count + index}",
-                  style: const TextStyle(fontSize: 60),
-                ),
-              ),
-            );
-          },
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: "Home",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.business),
-              label: "Buisiness",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.school),
-              label: "School",
-            ),
-          ],
-        ),
-        drawer: Drawer(
-          child: ListView.builder(
-            itemCount: 30,
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: Text('Item ${index + 1}'),
-                onTap: () {
-                  // Update the state of the app
-                  // ...
-                  // Then close the drawer
-                  Navigator.pop(context);
-                },
-              );
-            },
-          ),
+        body: Center(
+          child: Text(wordPair.asPascalCase),
         ),
       ),
     );
-  }
-
-  randomColor() {
-    return Color((math.Random().nextDouble() * 0xFFFFFF).toInt())
-        .withOpacity(1.0);
   }
 }
