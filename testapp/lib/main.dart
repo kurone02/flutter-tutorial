5609e8667350242c9d7d86cad329c1a53a242e90
kurone02 <fnt.minhduc@gmail.com>
2022-11-03 17:54:18 +0900

    Added saved names navigator

--- testapp/lib/main.dart
+++ testapp/lib/main.dart
@@ -11,16 +11,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: "Variable Name Generator",
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Variable Name Generator"),
-        ),
-        body: const Center(
-          child: RandomWords(),
-        ),
-      ),
+      home: RandomWords(),
     );
   }
 }
@@ -33,34 +26,94 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
+  final List<WordPair> suggestions = <WordPair>[];
+  final Set<WordPair> favourites = <WordPair>{};
+  TextStyle biggerFont = const TextStyle(fontSize: 18);
+
+  void pushSaved() {
+    Navigator.of(context).push(MaterialPageRoute<void>(builder: (context) {
+      final Iterable<ListTile> tiles = favourites.map(
+        (pair) {
+          return ListTile(
+            title: Text(
+              pair.asPascalCase,
+              style: biggerFont,
+            ),
+          );
+        },
+      );
+
+      final dynamic divided = tiles.isNotEmpty
+          ? ListTile.divideTiles(
+              context: context,
+              tiles: tiles,
+            )
+          : <Widget>[];
+
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text("Favourite names"),
+        ),
+        body: ListView(children: divided),
+      );
+    }));
+  }
+
   @override
   Widget build(BuildContext context) {
-    final List<WordPair> suggestions = <WordPair>[];
-    const TextStyle biggerFont = TextStyle(fontSize: 18);
-    return ListView.builder(
-      padding: const EdgeInsets.all(16),
-      itemBuilder: ((context, index) {
-        if (index.isOdd) {
-          return const Divider(
-            thickness: 2,
-          );
-        }
-
-        // If the index reaches half of the length, double the length
-        final int i = index ~/ 2; // Integer division
-        if (i >= suggestions.length) {
-          if (kDebugMode) {
-            print("ADDED MORE!");
-          }
-          suggestions.addAll(generateWordPairs().take(10));
-        }
-        return ListTile(
-          title: Text(
-            suggestions[i].asPascalCase,
-            style: biggerFont,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Variable Name Generator"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.list),
+            onPressed: pushSaved,
+            tooltip: "Favourites",
           ),
-        );
-      }),
+        ],
+      ),
+      body: ListView.builder(
+        padding: const EdgeInsets.all(16),
+        itemBuilder: ((context, index) {
+          if (index.isOdd) {
+            return const Divider(
+              thickness: 2,
+            );
+          }
+
+          // If the index reaches half of the length, double the length
+          final int i = index ~/ 2; // Integer division
+          if (i >= suggestions.length) {
+            if (kDebugMode) {
+              print("ADDED MORE!");
+            }
+            suggestions.addAll(generateWordPairs().take(10));
+          }
+
+          final bool isFavourite = favourites.contains(suggestions[i]);
+
+          return ListTile(
+            title: Text(
+              suggestions[i].asPascalCase,
+              style: biggerFont,
+            ),
+            trailing: Icon(
+              isFavourite ? Icons.favorite : Icons.favorite_border,
+              color: isFavourite ? Colors.red : null,
+              semanticLabel: isFavourite ? "Remove from favourites" : "Save",
+            ),
+            onTap: () {
+              setState(() {
+                if (isFavourite) {
+                  favourites.remove(suggestions[i]);
+                } else {
+                  favourites.add(suggestions[i]);
+                }
+              });
+            },
+          );
+        }),
+      ),
     );
   }
 }
